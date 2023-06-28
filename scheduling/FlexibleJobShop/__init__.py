d8f3a4b13733b81e949e5eeee6b3416ea632ba87
jose9808 <63043065+jose9808@users.noreply.github.com>
2023-06-28 09:39:35 +0200

    Update __init__.py

--- scheduling/FlexibleJobShop/__init__.py
+++ scheduling/FlexibleJobShop/__init__.py
@@ -1,1 +1,17 @@
+from . import core
+# from . import visualization
+# from . import benchmarking
+from . import instantiation
+from . import schedulers
+# from . import qubobuilders
 
+from .core import *
+# from .visualization import *
+# from .benchmarking import *
+from .instantiation import *
+
+__all__ = []
+__all__.extend(core.__all__)
+# __all__.extend(visualization.__all__)
+# __all__.extend(benchmarking.__all__)
+__all__.extend(instantiation.__all__)
